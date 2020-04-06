f0155bc4bfb5bf4a7fa53b5dff15128af231b6ad
Armaan Mohar <armaan.mohar@gmail.com>
2020-04-06 01:32:23 -0600

    some error checking and javadoc

--- PreProjectExercise/Actions.java
+++ PreProjectExercise/Actions.java
@@ -1,23 +1,55 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import javax.swing.JOptionPane;
 
+/**
+ * controls all backend functionality
+ * Initializes database
+ * Utilizes required functions from BinSearchTree
+ * @author A. Mohar, 
+ * @version 1.0
+ * @since April 6, 2020
+ * 
+ */
 public class Actions {
+    /**
+     * the tree
+     */
     private BinSearchTree myTree;
+
+    /**
+     * the frontend GUI
+     */
     private Graphics graphics;
 
+    /** 
+     * connects to frontend and initializes BinSearchTree
+     * @param g
+     */
     public Actions(Graphics g) {
         this.graphics = g;
         this.myTree = new BinSearchTree();
         // myTree.destroy();
     }
 
+    /**
+     * inserts new student into tree
+     * @param id
+     * @param fac
+     * @param major
+     * @param yr
+     */
     public void insert(String id, String fac, String major, String yr) {
         myTree.insert(id, fac, major, yr);
     }
 
+    /**
+     * searches for student info based on id input
+     * @param id
+     */
     public void find(int id) {
         String ID = String.valueOf(id);
         Node n = myTree.find(myTree.root, ID);
@@ -28,7 +60,12 @@ public class Actions {
         }
     }
 
-    public void createTree(String fileName) {
+    /**
+     * opens specified file and fills the tree with file data
+     * @param fileName
+     * @throws FileNotFoundException
+     */
+    public void createTree(String fileName) throws FileNotFoundException{
         try {
             Scanner sc = new Scanner(new FileReader(fileName));
             String line;
@@ -39,14 +76,24 @@ public class Actions {
                 insert(columns[0], columns[1], columns[2], columns[3]);
             }
             sc.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch(FileNotFoundException f){
+            JOptionPane.showMessageDialog(null, f.getMessage(), "File not found", JOptionPane.ERROR_MESSAGE);
         }
+         
     }
 
-    public void browse(PrintWriter printer) {
+    /**
+     * prints the whole tree out
+     * @param printer
+     * @throws IOException
+     */
+    public void browse(PrintWriter printer) throws IOException {
         try {
+            if(!myTree.empty()){
             myTree.print_tree(myTree.root, printer);
+            } else{
+                JOptionPane.showMessageDialog(null, "First create a tree to initialize database!", "Tree is empty!", JOptionPane.ERROR_MESSAGE);
+            }
         } catch (IOException e) {
             e.getStackTrace();
         }
--- PreProjectExercise/Graphics.java
+++ PreProjectExercise/Graphics.java
@@ -2,25 +2,59 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
+import java.util.IllegalFormatException;
 
 public class Graphics extends JFrame {
+    /**
+     * handles backend functions of buttons
+     */
     private Actions actions;
+
+    /**
+     * object from class BinSearchTree
+     */
     private PrintWriter printer;
+
+    /**
+     * 
+     */
     private JTextArea jta;
 
-    public Graphics(int wPixels, int hPixels) {
+    /**
+     * connects frontend(GUI) to backend starts the GUI
+     * 
+     * @param wPixels
+     * @param hPixels
+     */
+    public Graphics(int wPixels, int hPixels) throws Exception {
         actions = new Actions(this);
-        prepareGUI(wPixels, hPixels);
+        try {
+            prepareGUI(wPixels, hPixels);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    private void prepareGUI(int width, int height) {
+    /**
+     * Creates outer frame
+     * 
+     * @param width
+     * @param height
+     */
+    private void prepareGUI(int width, int height) throws Exception {
         setTitle("ProjectApplication");
         setSize(width, height);
         setLayout(new BorderLayout());
 
         add("North", createNPanel());
         add("Center", createCPanel());
-        add("South", createSPanel());
+        try {
+            add("South", createSPanel());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
         setVisible(true);
     }
@@ -50,7 +84,7 @@ public class Graphics extends JFrame {
         return p;
     }
 
-    private JPanel createSPanel() {
+    private JPanel createSPanel() throws Exception, IOException {
         JPanel p = new JPanel();
         p.setLayout(new FlowLayout());
 
@@ -72,50 +106,80 @@ public class Graphics extends JFrame {
             findGUI();
         });
         browse.addActionListener((ActionEvent e) -> {
-            browseGUI();
+            try {
+                browseGUI();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
         });
         createTree.addActionListener((ActionEvent e) -> {
-            createTreeGUI();
+            try {
+                createTreeGUI();
+            } catch (Exception e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
         });
         return p;
     }
 
-    private void browseGUI() {
+    /**
+     * @throws IOException
+     */
+    private void browseGUI() throws IOException {
         StringWriter buffer = new StringWriter();
         PrintWriter printer = new PrintWriter(buffer);
 
-        actions.browse(printer);
+        try {
+            actions.browse(printer);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         String content = buffer.toString();
         jta.append(content);
     }
 
-    private void createTreeGUI() {
+    /**
+     * prompts user for filename and then calls backend to load data from file to BinSearchTree
+     * @throws Exception
+     */
+    private void createTreeGUI() throws Exception {
         String filename = "";
+        String ext = ".txt";
         try {
             filename = JOptionPane.showInputDialog(null, "Please enter filename: ");
-            if (filename == null || filename == "") {
+            if (filename == null || filename == "" || !filename.toUpperCase().contains(ext.toUpperCase())) {
                 JOptionPane.showInputDialog(null, "File has to be in format: nameofFile.txt");
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Invalid filename");
         }
         actions.createTree(filename);
+
     }
 
-    private void findGUI() {
+    /**
+     * Graphical user interface which allows user to search for student by id
+     * @throws NumberFormatException
+     */
+    private void findGUI() throws NumberFormatException {
         int stuID = 0;
-        try {
-            stuID = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the Student's ID: "));
-            if (stuID < 1) {
-                JOptionPane.showMessageDialog(null, "ID cannot be less than one.");
+            try {
+                while(stuID < 1){
+                stuID = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the Student's ID: "));
+                }
+                actions.find(stuID);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Must be a numeric number!");
             }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Invalid ID.");
-        }
-        // check for the stuID in tree.
-        actions.find(stuID);
     }
 
+
+    /**
+     * prompts user for id, faculty, major, and year of new student
+     */
     private void insertGUI() {
         JPanel jp = new JPanel();
         JTextField stID = new JTextField(8);
@@ -134,14 +198,18 @@ public class Graphics extends JFrame {
         jp.add(new JLabel("Enter the year: "));
         jp.add(year);
 
+        
+        
+
         int res = JOptionPane.showConfirmDialog(null, jp, "Insert a new node", JOptionPane.OK_CANCEL_OPTION);
+        
         // checking the values
         if (res == JOptionPane.OK_OPTION) {
             actions.insert(stID.getText(), faculty.getText(), major.getText(), year.getText());
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new Graphics(600, 450);
     }
 }
