71149d6aac57da0d3da67d2ed856b136d7207a26
Armaan Mohar <armaan.mohar@gmail.com>
2020-04-09 16:06:36 -0600

    started sockets

--- CourseRegiSys/src/Client/ClientController/ClientCommunication.java
+++ CourseRegiSys/src/Client/ClientController/ClientCommunication.java
@@ -1,5 +1,12 @@
 package Client.ClientController;
 
-public class ClientCommunication {
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ClientCommunication {
+    private Socket aSocket;
+    private BufferedReader socketOut;   //msg to send to server
+    private PrintWriter socketIn;   //msg to read from socket
+    private BufferedReader stdIn;   //commands from client
 }
