287588ee99777d02a9a65e9d1b12685c410f53b1
Tom Pritchard <tom.pritchard1@ucalgary.ca>
2020-04-09 19:25:19 -0600

    implemented basic functionality of both communication classes

--- CourseRegiSys/src/Client/ClientController/ClientCommunication.java
+++ CourseRegiSys/src/Client/ClientController/ClientCommunication.java
@@ -1,6 +1,8 @@
 package Client.ClientController;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -8,5 +10,23 @@ public class ClientCommunication {
     private Socket aSocket;
     private BufferedReader socketOut;   //msg to send to server
     private PrintWriter socketIn;   //msg to read from socket
-    private BufferedReader stdIn;   //commands from client
+    
+    public ClientCommunication(String serverName, int port) {
+    	try {
+    		aSocket = new Socket(serverName, port);
+    		socketOut = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
+    		socketIn = new PrintWriter(aSocket.getOutputStream(), true);
+    	} catch (IOException e) {
+    		e.getStackTrace();
+    	}
+    }
+    
+    public void communicate() {
+    	
+    }
+    
+    public static void main(String[] args) {
+		ClientCommunication clientCom = new ClientCommunication("localhost", 9898);
+		clientCom.communicate();
+	}
 }
--- CourseRegiSys/src/Server/ServerController/ServerCommunication.java
+++ CourseRegiSys/src/Server/ServerController/ServerCommunication.java
@@ -1,10 +1,10 @@
 package Server.ServerController;
 
-import java.io.BufferedReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerCommunication {
     private ServerSocket serverSocket;  //server socket
@@ -12,4 +12,42 @@ public class ServerCommunication {
     private BufferedReader socketIn;    //msg to write to client
     private PrintWriter socketOut;  //msg to read from socket
     private ExecutorService pool;   //threadpool
+    
+    public ServerCommunication(int port) {
+    	try {
+    		serverSocket = new ServerSocket(port);
+    		pool = Executors.newCachedThreadPool();
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    public void runServer() {
+    	try {
+    		while(true) {
+    			aSocket = serverSocket.accept();
+    			System.out.println("Connection accepted by server!");
+    			socketIn = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
+    			socketOut = new PrintWriter(aSocket.getOutputStream(), true);
+    			
+    		}
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	closeConnection();
+    }
+    
+    public void closeConnection() {
+    	try {
+    		socketIn.close();
+    		socketOut.close();
+    	} catch (IOException e) {
+    		e.getStackTrace();
+    	}
+    }
+    
+    public static void main(String[] args) {
+		ServerCommunication serverCom = new ServerCommunication(9898);
+		serverCom.runServer();
+	}
 }
