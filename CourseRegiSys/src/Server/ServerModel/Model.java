09422de1e0d1aa18cad36b5970fb4b550b662538
Tom Pritchard <tom.pritchard1@ucalgary.ca>
2020-04-09 19:07:34 -0600

    Added field and constructor to ServerModel

--- CourseRegiSys/src/Server/ServerModel/Model.java
+++ CourseRegiSys/src/Server/ServerModel/Model.java
@@ -1,5 +1,12 @@
 package Server.ServerModel;
+import Server.ServerModel.Registration.*;
 
 public class Model {
 
+	private RegistrationApp regApp;
+	
+	public Model(CourseCatalogue cat) {
+		regApp = new RegistrationApp(cat);
+	}
+	
 }
